a061dda888d07d192a624611b8d715724bc9746f
nguyentruongson <nguyentruongson@DE62>
2024-09-17 13:00:08 +0700

    render children

---
+++ FamilyDiagram/Pages/DiagramWidgets/InvisibleNode.cs
@@ -0,0 +1,14 @@
+﻿using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+
+namespace FamilyDiagram.Pages.DiagramWidgets
+{
+    public class InvisibleNode : NodeModel
+    {
+        public InvisibleNode(Point? position = null) : base(position) { }
+
+        public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+
+    }
+}
--- FamilyDiagram/Pages/MyDiagram.razor.cs
+++ FamilyDiagram/Pages/MyDiagram.razor.cs
@@ -6,6 +6,7 @@ using Blazor.Diagrams.Core.Routers;
 using Blazor.Diagrams.Core.PathGenerators;
 using Blazor.Diagrams.Options;
 using FamilyDiagram.Model;
+using FamilyDiagram.Pages.DiagramWidgets;
 
 namespace FamilyDiagram.Pages;
 
@@ -77,16 +78,17 @@ public partial class MyDiagram
             AllowMultiSelection = true,
             Zoom =
             {
-                Enabled = false,
+                Enabled = true,
             },
             Links =
             {
-                DefaultRouter = new NormalRouter(),
-                DefaultPathGenerator = new SmoothPathGenerator()
+                DefaultRouter = new OrthogonalRouter(),
+                DefaultPathGenerator = new StraightPathGenerator()
             },
         };
 
         Diagram = new BlazorDiagram(options);
+        Diagram.RegisterComponent<InvisibleNode, InvisibleNodeWidget>();
         FamilyRender(familyTreeModel.Target, 0, null);
         //     var firstNode = Diagram.Nodes.Add(new NodeModel(position: new Point(50, 50))
         //     {
@@ -110,53 +112,78 @@ public partial class MyDiagram
         //     });
     }
     //Hien thi Family
-    int FamilyRender(Person nodeTarget, int Floor, Anchor? sourceAnchor)
+    double FamilyRender(Person nodeTarget, double Floor, PortModel? sourcePort)
     {
-        if (nodeTarget == null) return 0;
+        if (nodeTarget == null) return 0.0;
         var firstNode = Diagram.Nodes.Add(new NodeModel(position: new Point(nodeTarget.Layer * xSize, Floor * xSize))
         {
             Title = "Target"
         });
-        var targetAnchor = new ShapeIntersectionAnchor(firstNode);
-        if (sourceAnchor != null)
+        var targetLeftPort = firstNode.AddPort(PortAlignment.Left);
+        var targetRightPort = firstNode.AddPort(PortAlignment.Right);
+        var targetBottomPort = firstNode.AddPort(PortAlignment.Bottom);
+        if (sourcePort != null)
         {
-            var sourceLink = Diagram.Links.Add(new LinkModel(targetAnchor, sourceAnchor));
+            var sourceLink = Diagram.Links.Add(new LinkModel(targetLeftPort, sourcePort));
             sourceLink.PathGenerator = new StraightPathGenerator();
         }
         if (nodeTarget is HasFamilies nodeTargetHasFamilies)
         {
             if (nodeTargetHasFamilies.Families != null)
             {
-                var countFloor = FamilyMemberRender(nodeTargetHasFamilies.Families.First()!.Children!, Floor, targetAnchor);
                 if (nodeTargetHasFamilies.Families.First().Spouse != null)
                 {
+                    //Render Spouse
                     var secondNode = Diagram.Nodes.Add(new NodeModel(position: new Point(nodeTarget.Layer * xSize, (Floor + 1) * xSize))
                     {
                         Title = "Spouse"
                     });
-                    var spouseAnchor = new ShapeIntersectionAnchor(secondNode);
-                    var spouseLink = Diagram.Links.Add(new LinkModel(targetAnchor, spouseAnchor));
+                    var spousePort = secondNode.AddPort(PortAlignment.Top);
+                    //Render Paralell Link
+                    var spouseLink = Diagram.Links.Add(new LinkModel(targetBottomPort, spousePort)
+                    {
+                        Router = new NormalRouter()
+                    });
+                    //Render Invisible Node
+                    var invisibleNode = Diagram.Nodes.Add(new InvisibleNode(new Point(nodeTarget.Layer * xSize + 140, (Floor + 1) * xSize - 84)));//To-do
+                    var invisibleRightPort = invisibleNode.AddPort(PortAlignment.Right);
+                    var invisibleLeftPort = invisibleNode.AddPort(PortAlignment.Left);
+                    var invisibleAnchor = new SinglePortAnchor(invisibleLeftPort)
+                    {
+                        MiddleIfNoMarker = true,
+                    };
+
+                    //Render Invisible Anchor
+                    var sourceAnchor = new LinkAnchor(spouseLink, distance: 0.5, offsetX: 0, offsetY: 0);
+                    var invisibleLink = Diagram.Links.Add(new LinkModel(sourceAnchor, invisibleAnchor));
+
+                    //Render Children
+                    var countFloor = FamilyMemberRender(nodeTargetHasFamilies.Families.First()!.Children!, Floor + 0.5, invisibleRightPort);
                     return nodeTargetHasFamilies.Families.First()!.Children.Count() > 2 ? countFloor : Floor + 1;
                 }
-                return countFloor;
+                else
+                {
+                    var countFloor = FamilyMemberRender(nodeTargetHasFamilies.Families.First()!.Children!, Floor, targetRightPort);
+                    return countFloor;
+                }
             }
         }
         return Floor;
     }
-    int FamilyMemberRender(List<Child> children, int Floor, Anchor targetAnchor)
+    double FamilyMemberRender(List<Child> children, double Floor, PortModel sourcePort)
     {
         var tempFloor = Floor;
         foreach (var child in children)
         {
             Console.WriteLine(tempFloor);
-            var countFloor = 0;
+            var countFloor = 0.0;
             if (child == children.First())
             {
-                countFloor = FamilyRender(child, tempFloor, targetAnchor);
+                countFloor = FamilyRender(child, tempFloor, sourcePort);
             }
             else
             {
-                countFloor = FamilyRender(child, tempFloor + 1, targetAnchor);
+                countFloor = FamilyRender(child, tempFloor + 1, sourcePort);
             }
             tempFloor = countFloor;
         }
